c7f876603b464ba4ec1a5d638dd7ddb9c82d8b7a
LuisMcfly <luis.payares.ruiz@gmail.com>
2022-11-13 15:00:17 -0500

    Styled Components, consultando API de CryptoCompare

---
+++ src/data/monedas.js
@@ -0,0 +1,12 @@
+const monedas = [
+    {id: 'USD', nombre: 'Dolar de Estados Unidos'},
+    {id: 'COP', nombre: 'Pesos Colombianos'},
+    {id: 'MXN', nombre: 'Pesos Mexicanos'},
+    {id: 'EUR', nombre: 'Euro'},
+    {id: 'GBP', nombre: 'Libra Esterlina'},
+]
+
+export {
+    monedas,
+}
+   
